1f433846a6c758588935495dbe50ff8bd4050574
yu-miyahara <yu.miyahara@internnect.co.jp>
2020-07-25 18:00:54 +0900

    jsx syntax check

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return (<h1 className="App">Hello, World</h1>);
+  return (
+    <React.Fragment>
+      <lable htmlFor="bar">bar</lable>
+      <input type="text" onChange={() => {console.log("I am clicked")}} />
+    </React.Fragment>
+    )
 }
 
 export default App;
